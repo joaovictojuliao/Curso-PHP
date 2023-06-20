9d30d51e7cc01b48d8ee10b2ccd3734bb749d9b6
João Victor Julião <joaovictojuliao@hotmail.com>
2023-06-20 09:05:21 -0300

    Update index.php

--- ex002/index.php
+++ ex002/index.php
@@ -1,9 +1,11 @@
 <?php 
-    
+
     //constante - não pode ser alterada
     const PAIS = "Brasil";
 
-    //variavel - pode ser alterada
+    //variavel - pode ser alterada - sempre começam com $ 
     $nome = "Paulo";
+
+    //string, int, double, bool
     
 ?>
